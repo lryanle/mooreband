f8f165c8d6d21dd4656d2db5088cd7c0469a794a
Ryan Le <lryanle@users.noreply.github.com>
2019-05-10 03:31:53 -0500

    V1.0.0 pre-release 1
    -cleaned random stuff
    -added custom scrollbar (current bugs are known and will be dealt with soontm)
    -fixed typos ;<
    -fixed visual issues with the menu button on mobile
    -fixed header image messing up after scrolling down on mobile
    -fixed alignment on dropdown menus
    -fixed sections (removed/added/renamed)
    -added color guard director to staff list
    -moved some functions to the global scope for debugging
    -added a mobile device check function
    -added the debugColors() function to list the two color schemes used in the website (at the time of being called)

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
-function fader() {
-  var r = $('.blurred'),
-    wh = $(window).height(),
-    dt = $(document).scrollTop(),
-    elView, opacity;
-
-  // Loop elements with class "blurred"
-  r.each(function() {
-    elView = wh - ($(this).offset().top - dt + 200);
-    if (elView > 0) { // Top of DIV above bottom of window.
-      opacity = 1 / (wh + $(this).height()) * elView * 2
-      if (opacity < 1) // Bottom of DIV below top of window.
-        $(this).css('opacity', opacity);
-    }
-  });
+//DEVICE DETECTION
+var isMobile = false;
+if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) ||
+  /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0, 4))) {
+  isMobile = true;
 }
 
-$(document).bind('scroll', fader);
+//hex to rgba
+function hexToRgba(hex, alpha) {
+  var r = parseInt(hex.slice(1, 3), 16),
+    g = parseInt(hex.slice(3, 5), 16),
+    b = parseInt(hex.slice(5, 7), 16);
+
+  if (alpha) {
+    return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
+  } else {
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+  }
+}
 
 $(document).ready(function(){
 	$('a[href^="#"]').on('click',function (e) {
@@ -172,27 +173,15 @@ fetch('https://raw.githubusercontent.com/ghosh/uiGradients/master/gradients.json
 
   var x = Math.round(Math.random() * (colorSetTest.length - 1));
   var y = colorSetTest[x];
-  var c1 = y.colors[0];
-  var c2 = y.colors[1];
+  window.c1 = y.colors[0];
+  window.c2 = y.colors[1];
 
 function randomBackgroundOnLoad() {
   var x = Math.round(Math.random())
   if (x == 0) {
-    document.getElementById("parallax").setAttribute("style", "background:url(media/background.jpg) no-repeat center center; background-size: cover;")
+    document.getElementById("headerbg").setAttribute("style", "background:url(media/background.jpg) no-repeat center center; background-size: cover;")
   } else {
-    document.getElementById("parallax").setAttribute("style", "background:url(media/background2.jpg) no-repeat center center; background-size: cover;")
-  }
-}
-
-function hexToRgba(hex, alpha) {
-  var r = parseInt(hex.slice(1, 3), 16),
-    g = parseInt(hex.slice(3, 5), 16),
-    b = parseInt(hex.slice(5, 7), 16);
-
-  if (alpha) {
-    return "rgba(" + r + ", " + g + ", " + b + ", " + alpha + ")";
-  } else {
-    return "rgb(" + r + ", " + g + ", " + b + ")";
+    document.getElementById("headerbg").setAttribute("style", "background:url(media/background2.jpg) no-repeat center center; background-size: cover;")
   }
 }
 
@@ -231,4 +220,36 @@ function footer() {
 })();
 
 document.onload = randomBackgroundOnLoad(), homeHeaderGradient(), cursorColor(), footer();
+  return c1, c2;
 });
+
+function mobileCompat() {
+  if (isMobile == true) {
+    if (this.scrollY < 180) {
+      if (($('.navbar-collapse').attr('aria-expanded') === "false") || ($('.navbar-collapse').attr('aria-expanded') === undefined)) {
+        console.log("hey bit");
+        document.getElementById("nav").setAttribute("style", `background: linear-gradient(200deg, ${hexToRgba(c1, 0.75)} 0%, ${hexToRgba(c2, 0.75)} 100%);`)
+        $('.navbar').addClass('nav--sticky');
+      } else if ($('.navbar-collapse').attr('aria-expanded') === "true") {
+        document.getElementById("nav").setAttribute("style", ``)
+        $('.navbar').removeClass('nav--sticky');
+      }
+    }
+  }
+}
+
+// <DEBUG>
+function debugColors() {
+  function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+    return (rgb && rgb.length === 4) ? "#" +
+      ("0" + parseInt(rgb[1], 10).toString(16)).slice(-2) +
+      ("0" + parseInt(rgb[2], 10).toString(16)).slice(-2) +
+      ("0" + parseInt(rgb[3], 10).toString(16)).slice(-2) : '';
+  }
+  let rgb = "rgb"
+  let color1 = rgb2hex(rgb.concat(document.getElementById("home").getAttribute("style").slice(40).split('%, ')[0].split(', 0.75) 0')[0].concat(')')))
+  let color2 = rgb2hex(rgb.concat(document.getElementById("home").getAttribute("style").slice(36).split('%, ')[1].split(' 100%);')[0].slice(4)).split(', 0.75)')[0].concat(')'))
+  console.log(`First Color: ${color1}\nSecond Color: ${color2}`);
+}
+// </DEBUG>
